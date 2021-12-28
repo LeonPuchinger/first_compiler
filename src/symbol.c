be6a0ab732d277ff61c418407eb6a1033a9c998e
LeonPuchinger <lepu1590@gmail.com>
2021-12-28 01:54:03 +0100

    implement symbol

--- src/symbol.c
+++ src/symbol.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "symbol.h"
 
+//list
+
 List_Container *new_list_container(void *item) {
     List_Container *new = malloc(sizeof(List_Container));
     new->item = item;
@@ -44,4 +46,17 @@ void list_add(List *list, void *item) {
         list->current->next = container;
     }
     list->current = container;
+}
+
+//symbol table
+
+Symbol *new_symbol(Symbol_Type type, Token *name) {
+    Symbol *new = malloc(sizeof(Symbol));
+    new->type = type;
+    new->name = name;
+    return new;
+}
+
+void free_symbol(Symbol *symbol) {
+    free(symbol);
 }
