2ac5e6e632852f532406d1c5dcd405cac8398160
LeonPuchinger <lepu1590@gmail.com>
2022-02-01 00:08:38 +0100

    test symbol table scopes

--- test/src/test_symbol.c
+++ test/src/test_symbol.c
@@ -9,26 +9,67 @@ Symbol_Table *empty_table() {
     return new_symbol_table();
 }
 
+Symbol *symbol_ident(char *token_content, int token_size) {
+    Token *a_tk = new_token(TK_IDENT, token_content, token_size);
+    return new_symbol(SYM_INT, a_tk);
+}
+
 //Tests
 
-int test_set() {
+int test_set_get() {
     int err;
     Symbol_Table *table = empty_table();
 
-    Token *a_tk = new_token(TK_IDENT, "a", 1);
-    Symbol *a_sym = new_symbol(SYM_INT, a_tk);
+    Symbol *a_sym = symbol_ident("a", 1);
     symbol_table_set(table, a_sym);
-    Symbol *a_return = symbol_table_get(table, a_tk);
+    Symbol *a_return = symbol_table_get(table, a_sym->name);
     err = assert(a_sym, a_return);
     if (err) return err;
 
     return 0;
 }
 
+int test_scopes() {
+    int err;
+    Symbol_Table *table = empty_table();
+
+    Symbol *a_sym = symbol_ident("a", 1);
+    Symbol *b_sym = symbol_ident("b", 1);
+    Symbol *a_return, *b_return;
+
+    symbol_table_set(table, a_sym);
+    symbol_table_push(table);
+    symbol_table_set(table, b_sym);
+
+    //a should be available in pushed scope
+    a_return = symbol_table_get(table, a_sym->name);
+    err = assert(a_return, a_sym);
+    if (err) return err;
+
+    //b should be available in pushed scope
+    b_return = symbol_table_get(table, b_sym->name);
+    err = assert(b_return, b_sym);
+    if (err) return err;
+
+    symbol_table_pop(table);
+
+    //a should be available in root scope
+    a_return = symbol_table_get(table, a_sym->name);
+    err = assert(a_return, a_sym);
+    if (err) return err;
+
+    //b should not be available in root scope
+    b_return = symbol_table_get(table, b_sym->name);
+    err = assert(b_return, NULL);
+    if (err) return err;
+
+    return 0;
+}
 
 int main() {
     gather_tests(
-        test_set,
+        test_set_get,
+        test_scopes,
         NULL
     );
 }
