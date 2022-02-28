4d8a91f057174092be9037cbba441e6a8e06f3c3
Leon Puchinger <lepu1590@gmail.com>
2022-02-28 11:27:24 +0100

    codegen write composite initial assign

--- src/codegen.c
+++ src/codegen.c
@@ -8,7 +8,7 @@
 #define REGISTER_SIZE 8 //64-bit ^= 8 byte
 
 static int current_indent = 0;
-//amount of bytes on the stack in addition to vars (e.g. return addrs)
+//amount of bytes on the stack in addition to vars (e.g. return addrs) in bytes
 static int current_stack_addr_offset = 0;
 
 //varargs style version of writef
@@ -70,17 +70,74 @@ void assign_addrs(Symbol_Table *table) {
     _assign_addrs(table->root_scope, 0);
 }
 
+//converts "virtual" symbol-table addr to stack addr, relative to rbp
+//relative to rbp means the addr is n bytes lower than rbp
+int stack_addr(int virtual_addr) {
+    return (current_stack_addr_offset + virtual_addr) * REGISTER_SIZE;
+}
+
+int write_assign(AST_Node *assignment, Symbol_Table *table, FILE *out_file) {
+    AST_Node *assignee = assignment->lhs;
+    AST_Node *expr = assignment->rhs;
+    int is_initial_assign = symbol_table_is_local(table, assignee->token);
+    int is_composite = expr->node_type == ND_ADD || expr->node_type == ND_SUB;
+    if (is_initial_assign) {
+        if (is_composite) {
+            //init = a +/- b
+            writef(out_file, "mov rax ");
+            if (expr->lhs->node_type == ND_VAR) {
+                //init = var +/- ...
+                int addr = stack_addr(symbol_table_get(table, expr->lhs->token)->addr);
+                writelnf(out_file, "rbp - %d", addr);
+            }
+            else {
+                //init = const +/- ...
+                char *constant = expr->lhs->token->value;
+                writelnf(out_file, "%s", constant);
+            }
+            //write operator
+            if (expr->node_type == ND_ADD) {
+                writef(out_file, "add rax ");
+            }
+            else {
+                writef(out_file, "sub rax ");
+            }
+            if (expr->rhs->node_type == ND_VAR) {
+                //init = ... +/- var
+                int addr = stack_addr(symbol_table_get(table, expr->rhs->token)->addr);
+                writelnf(out_file, "rbp - %d", addr);
+            }
+            else {
+                //init = ... +/- const
+                char *constant = expr->rhs->token->value;
+                writelnf(out_file, "%s", constant);
+            }
+            writelnf(out_file, "push rax");
+        }
+    }
+    else {
+        int assignee_addr = stack_addr(symbol_table_get(table, assignee->token)->addr);
+
+    }
+    return 0;
+}
+
 int write_statements(AST_Node *statements, Symbol_Table *table, FILE *out_file) {
     AST_Node *current_statement = statements;
     while (current_statement != NULL) {
         if (current_statement->node_type == ND_ASSIGN) {
-            //TODO write assign
+            int err = write_assign(current_statement, table, out_file);
+            if (err) return 1;
         }
         else if (current_statement->node_type == ND_FUNCTION_DEF) {
             //TODO write func def
+            printf("NOT IMPLEMENTED\n");
+            return 1;
         }
         else if (current_statement->node_type == ND_FUNCTION_CALL) {
             //TODO write func call
+            printf("NOT IMPLEMENTED\n");
+            return 1;
         }
         else {
             printf("ERROR: AST_Node is not a statement\n");
